83227ef503eb32adc50d4447e6888d4f575963cb
madosuki <yurikasamaaikatsu@gmail.com>
2016-09-22 11:46:03 +0900

    Site updated: 2016-09-22 11:46:02

--- js/script.js
+++ js/script.js
@@ -1,71 +1,47 @@
 (function($){
-  /*toTop start*/
-  // When to show the scroll link
-  // higher number = scroll link appears further down the page
-  var upperLimit = 1000;
-  // Our scroll link element
-  var scrollElem = $('#totop');
-  // Scroll to top speed
-  var scrollSpeed = 500;
-  // Show and hide the scroll to top link based on scroll position
-  $(window).scroll(function() {
-    var scrollTop = $(document).scrollTop();
-    if (scrollTop > upperLimit) {
-      $(scrollElem).stop().fadeTo(300, 1); // fade back in
-    } else {
-      $(scrollElem).stop().fadeTo(300, 0); // fade out
-    }
-  });
+  // Search
+  var $searchWrap = $('#search-form-wrap'),
+    isSearchAnim = false,
+    searchAnimDuration = 200;
 
-  // Scroll to top animation on click
-  $(scrollElem).click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, scrollSpeed);
-    return false;
-  });
-  /*toTop end*/
-
-  /*cubeRotate start*/
-  var isIE = function(){
-	return ("ActiveXObject" in window);
+  var startSearchAnim = function(){
+    isSearchAnim = true;
   };
-  if( isIE() ) {
-    $('#contenedor').hide();
-  } else {
-    var cube = $('.cube'),
-        offset = $('#contenedor').offset(),
-        offsetleft = (offset.left + 50),
-        offsettop = (offset.top + 50);
 
-    cube.on({
-      mousemove: function(e) {
-        $(this).css('transform','rotateX(' + (e.pageY - offsettop) + 'deg) rotateY(' + (e.pageX - offsetleft) + 'deg)');
-        $(this).addClass('noanimar').removeClass('animar');
-      },
-      mouseout: function() {
-        $(this).css('transform','rotateX(-25deg) rotateY(32deg)');
-        $(this).addClass('animar').removeClass('noanimar');
-      }
+  var stopSearchAnim = function(callback){
+    setTimeout(function(){
+      isSearchAnim = false;
+      callback && callback();
+    }, searchAnimDuration);
+  };
+
+  $('#nav-search-btn').on('click', function(){
+    if (isSearchAnim) return;
+
+    startSearchAnim();
+    $searchWrap.addClass('on');
+    stopSearchAnim(function(){
+      $('.search-form-input').focus();
     });
-    // console.log('x=' + offsetleft + ', y=' + offsettop);
-  }
-  /* cubeRotate end*/
+  });
+
+  $('.search-form-input').on('blur', function(){
+    startSearchAnim();
+    $searchWrap.removeClass('on');
+    stopSearchAnim();
+  });
 
   // Share
-  $('body').on('click', function(){
+  /*$('body').on('click', function(){
     $('.article-share-box.on').removeClass('on');
-    $('.qrcode').hide();
   }).on('click', '.article-share-link', function(e){
     e.stopPropagation();
 
     var $this = $(this),
-      type = $this.attr('data-share'),
+      url = $this.attr('data-url'),
+      encodedUrl = encodeURIComponent(url),
+      id = 'article-share-box-' + $this.attr('data-id'),
       offset = $this.offset();
-
-    var url = $this.attr('data-url'),
-    encodedUrl = encodeURIComponent(url),
-    id = 'article-share-box-' + $this.attr('data-id');
 
     if ($('#' + id).length){
       var box = $('#' + id);
@@ -79,14 +55,10 @@
         '<div id="' + id + '" class="article-share-box">',
           '<input class="article-share-input" value="' + url + '">',
           '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-            '<a href="https://www.linkedin.com/shareArticle?url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="Linkedin"></a>',
-            '<a href="http://service.weibo.com/share/share.php?url=' + encodedUrl + '" class="article-share-weibo" target="_blank" title="Weibo"></a>',
-            '<a href="http://share.renren.com/share/buttonshare.do?link=' + encodedUrl + '" class="article-share-renren" target="_blank" title="Renren"></a>',
-            '<a href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=' + encodedUrl + '" class="article-share-qq" target="_blank" title="Qzone"></a>',
-            '<a class="article-share-weixin" target="_blank" title="Weixin"></a>',
+			'<a href="http://tieba.baidu.com/f/commit/share/openShareApi?url=' + encodedUrl + '" class="article-share-tieba" target="_blank" title="百度贴吧"></a>',
+			'<a href="http://service.weibo.com/share/share.php?url=' + encodedUrl + '" class="article-share-weibo" target="_blank" title="新浪微博"></a>',
+			'<a href="http://share.v.t.qq.com/index.php?c=share&a=index&url=' + encodedUrl + '" class="article-share-tqq" target="_blank" title="腾讯微博"></a>',
+			'<a href="http://widget.renren.com/dialog/share?resourceUrl=' + encodedUrl + '" class="article-share-renren" target="_blank" title="人人"></a>',
           '</div>',
         '</div>'
       ].join('');
@@ -96,30 +68,12 @@
       $('body').append(box);
     }
 
+    $('.article-share-box.on').hide();
+
     box.css({
       top: offset.top + 25,
       left: offset.left
     }).addClass('on');
-
-    $('.article-share-weixin').click(function(){
-      var e_qrcode = $(this).parents('.article-share-box').next('.qrcode');
-      if (e_qrcode.length){
-        e_qrcode.show();
-      } else {
-        $(this).parents('.article-share-box').after('<div class="qrcode"></div>');
-        e_qrcode = $(this).parents('.article-share-box').next('.qrcode');
-        e_qrcode.qrcode({
-          "render": "div",
-          "size": 180,
-          "text": encodeURI(url)
-        });
-        e_qrcode.css({
-          top: offset.top + 20,
-          left: offset.left -150,
-        });
-      }
-    });
-
   }).on('click', '.article-share-box', function(e){
     e.stopPropagation();
   }).on('click', '.article-share-box-input', function(){
@@ -129,7 +83,7 @@
     e.stopPropagation();
 
     window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
+  });*/
 
   // Caption
   $('.article-entry').each(function(i){
@@ -180,4 +134,16 @@
 
     $container.removeClass('mobile-nav-on');
   });
+  
+    // link
+    var $linkUl = $('#link-list');
+    var $list = $('#link-list li');
+    $linkUl.empty();
+    var count = $list.length;
+    for(var i = 0; i < count; i++)
+    {
+        var ran = Math.floor(Math.random() * $list.length);
+        $linkUl.append($list.eq(ran));
+        $list.splice(ran, 1);
+    }
 })(jQuery);
